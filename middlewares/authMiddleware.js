d714620aef0c69dcf6f161bcc57516a6b7709bef
UmarSE2 <umer.se02@gmail.com>
2024-04-14 20:46:04 +0500

    Check user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModal');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt
@@ -20,4 +21,26 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth }
+const checkUser = async (req, res, next) => {
+    const token = req.cookies.jwt
+    if (token) {
+        jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message)
+                res.locals.user = null
+                next()
+            } else {
+                console.log(decodedToken)
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user
+                next()
+            }
+        })
+    }
+    else {
+        res.locals.user = null
+        next()
+    }
+}
+
+module.exports = { requireAuth, checkUser }
